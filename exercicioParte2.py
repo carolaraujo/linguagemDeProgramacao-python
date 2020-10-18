679d7cecf4988a34ce5bfb443b56b4e7dd656bc6
Caroline Araujo <ca.araujosouza@gmail.com>
2020-10-17 22:26:29 -0300

    exercicios unidade 1 parte 2

--- exercicioParte2.py
+++ exercicioParte2.py
@@ -2,12 +2,12 @@
 # Sobre o valor vendido, o fabricante paga 40% de imposto. Escreva um programa
 # que calcule o valor do imposto a ser pago
 
-# valor_original = float(input("valor original: R$ "))
-# desconto = float(input("Desconto (em %) para esse produto: "))
-# desconto = desconto / 100
-# print("valor original:    R$", valor_original)
-# print("Desconto ganho:    R$", valor_original * desconto)
-# print("valor com desconto:    R$", valor_original * (1-desconto))
+valor_original = float(input("valor original: R$ "))
+desconto = float(input("Desconto (em %) para esse produto: "))
+desconto = desconto / 100
+print("valor original:    R$", valor_original)
+print("Desconto ganho:    R$", valor_original * desconto)
+print("valor com desconto:    R$", valor_original * (1-desconto))
 
 # Escreva um programa que calcule o rendimento em km/l de um automóvel
 # Sendo dados: a quantidade de litros de combústivel gasta é igual a 43 litros
